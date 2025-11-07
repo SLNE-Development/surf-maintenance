e28252cdfc7e61f038f8cc004d82e02f161b36cd
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-11-07 15:58:12 +0100

    feat: Update maintenance MOTD to include server information and Discord link

--- surf-maintenance-velocity/src/main/kotlin/dev/slne/surf/maintenance/velocity/config/MaintenanceConfig.kt
+++ surf-maintenance-velocity/src/main/kotlin/dev/slne/surf/maintenance/velocity/config/MaintenanceConfig.kt
@@ -7,7 +7,8 @@ import org.spongepowered.configurate.objectmapping.meta.Comment
 data class MaintenanceConfig(
     var enabled: Boolean = false,
     @param:Comment("The MOTD to display when the server is in maintenance mode")
-    val maintenanceMotd: String = "",
+    val maintenanceMotd: String = " <#6fa8dc> CASTCRAFTER.DE </#6fa8dc><gray>- </gray><red>WARTUNGSARBEITEN</red><gray> - </gray><white>[</white><light_purple>1.21.10</light_purple><white>]\n" +
+            "</white><red>     Weitere Informationen findest du im Discord.",
     @param:Comment("The kick message to display when a player is kicked due to maintenance mode, contains surf formatting")
     val maintenanceKickMessage: String = "",
     val maxPlayerCount: Int = 0,
