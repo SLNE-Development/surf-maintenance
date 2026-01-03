cc4e561ca73998cbfac373a4b7753a7b72432e1e
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-01-03 16:29:47 +0100

    refactor: Update Gradle configuration and dependencies for maintenance module adjustments

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ buildscript {
         maven("https://repo.slne.dev/repository/maven-public/") { name = "maven-public" }
     }
     dependencies {
-        classpath("dev.slne.surf:surf-api-gradle-plugin:1.21.10+")
+        classpath("dev.slne.surf:surf-api-gradle-plugin:1.21.11+")
     }
 }
 
 allprojects {
-    group = "dev.slne.surf.template"
+    group = "dev.slne.surf.maintenance"
     version = findProperty("version") as String
 }
--- surf-maintenance-velocity/build.gradle.kts
+++ surf-maintenance-velocity/build.gradle.kts
@@ -2,11 +2,11 @@ plugins {
     id("dev.slne.surf.surfapi.gradle.velocity")
 }
 
+surfVelocityApi {
+    withSurfRedis()
+}
+
 velocityPluginFile {
     main = "dev.slne.surf.maintenance.velocity.VelocityMain"
     authors = listOf("red")
-}
-
-dependencies {
-    implementation("dev.slne.surf:surf-redis:1.0.0-SNAPSHOT")
 }
