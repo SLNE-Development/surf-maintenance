41804f7c2cfe6a33913537c4ffff4d5a6c5800b4
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-01-19 15:25:41 +0100

    feat: Add MaintenanceKickRedisEvent and implement player kick functionality during maintenance mode sessions

--- surf-maintenance-velocity/src/main/kotlin/dev/slne/surf/maintenance/velocity/command/MaintenanceCommand.kt
+++ surf-maintenance-velocity/src/main/kotlin/dev/slne/surf/maintenance/velocity/command/MaintenanceCommand.kt
@@ -5,13 +5,11 @@ import dev.jorel.commandapi.kotlindsl.commandTree
 import dev.jorel.commandapi.kotlindsl.literalArgument
 import dev.slne.surf.maintenance.velocity.configuration
 import dev.slne.surf.maintenance.velocity.plugin
+import dev.slne.surf.maintenance.velocity.redis.event.MaintenanceKickRedisEvent
 import dev.slne.surf.maintenance.velocity.redis.event.MaintenanceStatusChangeRedisEvent
 import dev.slne.surf.maintenance.velocity.redisApi
 import dev.slne.surf.maintenance.velocity.util.MaintenancePermissions
-import dev.slne.surf.surfapi.core.api.messages.CommonComponents
-import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
 import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
-import java.util.*
 
 fun maintenanceCommand() = commandTree("maintenance") {
     withPermission(MaintenancePermissions.MAINTENANCE_COMMAND)
@@ -90,10 +88,12 @@ fun maintenanceCommand() = commandTree("maintenance") {
                 return@anyExecutor
             }
 
+            redisApi.publishEvent(MaintenanceKickRedisEvent())
+
             executor.sendText {
                 appendPrefix()
                 success("Es wurden ")
-                variableValue(kickPlayers().size)
+                variableValue("(Ich weiß nicht wie viele Spieler, aber bestimmt ein paar)")
                 success(" Spieler gekickt.")
             }
         }
@@ -109,24 +109,4 @@ fun maintenanceCommand() = commandTree("maintenance") {
             }
         }
     }
-}
-
-private fun kickPlayers(): List<UUID> {
-    val list = mutableListOf<UUID>()
-
-    plugin.proxy.allPlayers.forEach {
-        if (!it.hasPermission(MaintenancePermissions.MAINTENANCE_BYPASS)) {
-            it.disconnect(buildText {
-                appendDisconnectMessage("DER SERVER BEFINDET SICH IM WARTUNGSMODUS", {
-                    variableValue("Es werden nun Wartungen am Server durchgeführt.")
-                    appendNewline()
-                    spacer("Weitere Informationen findest du in unserem Discord.")
-                }, {
-                    append(CommonComponents.RETRY_LATER_FOOTER)
-                })
-            })
-            list.add(it.uniqueId)
-        }
-    }
-    return list
 }
--- surf-maintenance-velocity/src/main/kotlin/dev/slne/surf/maintenance/velocity/redis/listener/event/MaintenanceRedisListener.kt
+++ surf-maintenance-velocity/src/main/kotlin/dev/slne/surf/maintenance/velocity/redis/listener/event/MaintenanceRedisListener.kt
@@ -1,12 +1,33 @@
 package dev.slne.surf.maintenance.velocity.redis.listener.event
 
 import dev.slne.surf.maintenance.velocity.plugin
+import dev.slne.surf.maintenance.velocity.redis.event.MaintenanceKickRedisEvent
 import dev.slne.surf.maintenance.velocity.redis.event.MaintenanceStatusChangeRedisEvent
+import dev.slne.surf.maintenance.velocity.util.MaintenancePermissions
 import dev.slne.surf.redis.event.OnRedisEvent
+import dev.slne.surf.surfapi.core.api.messages.CommonComponents
+import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
 
 object MaintenanceRedisListener {
     @OnRedisEvent
     fun onMaintenanceStatusChange(event: MaintenanceStatusChangeRedisEvent) {
         plugin.enabled = event.enabled
+    }
+
+    @OnRedisEvent
+    fun onMaintenanceKick(event: MaintenanceKickRedisEvent) {
+        plugin.proxy.allPlayers.forEach {
+            if (!it.hasPermission(MaintenancePermissions.MAINTENANCE_BYPASS)) {
+                it.disconnect(buildText {
+                    appendDisconnectMessage("DER SERVER BEFINDET SICH IM WARTUNGSMODUS", {
+                        variableValue("Es werden nun Wartungen am Server durchgeführt.")
+                        appendNewline()
+                        spacer("Weitere Informationen findest du in unserem Discord.")
+                    }, {
+                        append(CommonComponents.RETRY_LATER_FOOTER)
+                    })
+                })
+            }
+        }
     }
 }
